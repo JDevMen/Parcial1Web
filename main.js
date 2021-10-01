5986e79c1afa561120ff569433b9a351d8e6293f
JDevMen <jdmendoza_90@hotmail.com>
2021-09-30 19:16:55 -0500

    Funcionalidad botón más y menos

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-let cantidadProductos = 0;
+const data = "./restaurant.json";
+
+//Elementos html dentro de index.html-------------------------
 let carrito = document.getElementById("carroCompra");
 let items = document.getElementById("items");
 let sep = document.getElementById("separador");
@@ -7,16 +9,27 @@ let taco = document.getElementById("tacos");
 let salad = document.getElementById("salads");
 let dessert = document.getElementById("desserts");
 let drink = document.getElementById("drinks");
-
 let categoria = document.getElementById("categoriaActual");
-
 let tableDiv = document.getElementById("tableDiv");
 
-const data = "./restaurant.json";
-
+//Variables auxiliares----------------------
+let cantidadProductos = 0;
 let cart = [];
 let array = [];
 
+//Funciones de cálculo o modificación------------------------
+//Función obtener total de orden
+function getTotal() {
+  let t = 0;
+
+  cart.forEach((element) => {
+    t += element.amount;
+  });
+
+  return t;
+}
+
+//Función para agregar item al carrito
 function agregarCarrito(item) {
   let name = item.name;
   let found = cart.find((i) => i["description"] === name);
@@ -38,14 +51,38 @@ function agregarCarrito(item) {
   console.log(cart);
 }
 
-function clearCards() {
-  let cartas = document.getElementById("cartas");
-  while (cartas.firstChild) {
-    cartas.removeChild(cartas.firstChild);
-  }
+//Función agregar cantidad a item en carrito
+function masItem(item, htmlQty, htmlAmount) {
+  item.qty++;
+  item.amount += item.unitPrice;
+  htmlQty.innerHTML = item.qty;
+  htmlAmount.innerHTML = item.amount;
+  let total = document.getElementById("totalB");
+  total.innerHTML = "Total: $" + getTotal();
+  items.innerHTML = cart.length + " items";
 }
 
-//Función creación de tarjetas
+//Función quitar cantidad a item en carrito
+function menosItem(item, htmlQty, htmlAmount) {
+  item.qty--;
+  let indexItem = cart.indexOf(item);
+  let total = document.getElementById("totalB");
+  if (item.qty > 0) {
+    item.amount -= item.unitPrice;
+    htmlQty.innerHTML = item.qty;
+    htmlAmount.innerHTML = item.amount;
+  } else {
+    cart.splice(indexItem, 1);
+    clearTable();
+    orderDetail();
+  }
+  total.innerHTML = "Total: $" + getTotal();
+  items.innerHTML = cart.length + " items";
+}
+
+//------------------Funciones de creación--------------------
+
+//Función crear de tarjetas
 function createCards(n) {
   fetch(data)
     .then((res) => res.json())
@@ -110,8 +147,9 @@ function createCards(n) {
 //Función crear tabla de orden
 function orderDetail() {
   let table = document.createElement("table");
-  table.className = "table table-bordered table-striped table-hover";
+  table.className = "table table-striped";
 
+  //Table head
   let tableHead = document.createElement("thead");
   let trHead = document.createElement("tr");
 
@@ -148,18 +186,104 @@ function orderDetail() {
 
   tableHead.appendChild(trHead);
 
+  //Table body
   let tableBody = document.createElement("tbody");
+  console.log("cart", cart);
+  for (let i = 0; i < cart.length; i++) {
+    const element = cart[i];
+    console.log("element", element);
+    let newRow = tableBody.insertRow();
+    let item = newRow.insertCell();
+    let qty = newRow.insertCell();
+    let desc = newRow.insertCell();
+    let unit = newRow.insertCell();
+    let amount = newRow.insertCell();
+    let modify = newRow.insertCell();
+
+    item.innerHTML = i + 1;
+    qty.innerHTML = element.qty;
+    desc.innerHTML = element.description;
+    unit.innerHTML = element.unitPrice;
+    amount.innerHTML = element.amount;
+
+    let botonMenos = document.createElement("button");
+    botonMenos.type = "button";
+    botonMenos.className = "btn btn-secondary adding";
+    botonMenos.innerHTML = "-";
+    botonMenos.addEventListener("click", () => menosItem(element, qty, amount));
+
+    let botonMas = document.createElement("button");
+    botonMas.type = "button";
+    botonMas.className = "btn btn-secondary adding";
+    botonMas.innerHTML = "+";
+    botonMas.addEventListener("click", () => masItem(element, qty, amount));
+
+    modify.appendChild(botonMenos);
+    modify.appendChild(botonMas);
+  }
 
   //Agregar tHead y tbody
   table.appendChild(tableHead);
+  table.appendChild(tableBody);
 
   tableDiv.appendChild(table);
 
+  orderFunction();
   console.log(tableDiv);
 }
 
-//Event listeners categorias
+//Función crear total y botones de orden
+function orderFunction() {
+  let row = document.createElement("div");
+  row.className = "row";
+
+  let total = document.createElement("div");
+  total.className = "col-8";
+
+  let b = document.createElement("b");
+  b.id = "totalB";
+  b.innerText = "Total: $" + getTotal();
+
+  total.appendChild(b);
+
+  let botones = document.createElement("div");
+  botones.className = "col-4";
+
+  let cancel = document.createElement("button");
+  cancel.type = "button";
+  cancel.className = "btn btn-danger";
+  cancel.innerHTML = "Cancel";
+
+  let confirm = document.createElement("button");
+  confirm.type = "button";
+  confirm.className = "btn btn-light";
+  confirm.innerHTML = "Confirm order";
+
+  botones.appendChild(cancel);
+  botones.appendChild(confirm);
+
+  row.appendChild(total);
+  row.appendChild(botones);
+
+  tableDiv.appendChild(row);
+}
+
+//Funciones para eliminar elementos tarjetas y tabla
+function clearCards() {
+  let cartas = document.getElementById("cartas");
+  while (cartas.firstChild) {
+    cartas.removeChild(cartas.firstChild);
+  }
+}
+function clearTable() {
+  while (tableDiv.firstChild) {
+    tableDiv.removeChild(tableDiv.firstChild);
+  }
+}
+
+//--------------------Event listeners categorias-------------
 burger.addEventListener("click", () => {
+  clearTable();
   clearCards();
   separador.hidden = false;
   categoria.hidden = false;
@@ -168,6 +292,7 @@ burger.addEventListener("click", () => {
 });
 
 taco.addEventListener("click", () => {
+  clearTable();
   clearCards();
   separador.hidden = false;
   categoria.hidden = false;
@@ -175,6 +300,7 @@ taco.addEventListener("click", () => {
   createCards(1);
 });
 salad.addEventListener("click", () => {
+  clearTable();
   clearCards();
   separador.hidden = false;
   categoria.hidden = false;
@@ -182,6 +308,7 @@ salad.addEventListener("click", () => {
   createCards(2);
 });
 dessert.addEventListener("click", () => {
+  clearTable();
   clearCards();
   separador.hidden = false;
   categoria.hidden = false;
@@ -189,15 +316,16 @@ dessert.addEventListener("click", () => {
   createCards(3);
 });
 drink.addEventListener("click", () => {
+  clearTable();
   clearCards();
   separador.hidden = false;
   categoria.hidden = false;
   categoria.innerHTML = "Drinks & Sides";
   createCards(4);
 });
-
 //Event listener carrito
 carrito.addEventListener("click", () => {
+  clearTable();
   separador.hidden = false;
   categoria.hidden = false;
   categoria.innerHTML = "Order detail";
